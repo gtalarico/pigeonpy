4cbb79bd9f80989733c677fadb27b8e891bc1130
Gui Talarico <gtalarico@gmail.com>
2017-03-15 02:41:17 -0400

    Reverted Bucket Controller to working version

--- pigeonpie/static/js/bucketsCtrl.js
+++ pigeonpie/static/js/bucketsCtrl.js
@@ -3,8 +3,8 @@
     'use strict';
 
     angular.module('PigeonPieApp')
-        .controller('bucketsController', ['$scope', '$log', '$http', '$timeout', '$window',
-            function($scope, $log, $http, $timeout, $window) {
+        .controller('bucketsController', ['$scope', '$log', '$http', '$timeout',
+            function($scope, $log, $http, $timeout) {
 
                 // Get Bucket List
                 var timeout = "";
@@ -13,11 +13,12 @@
                         .then(function successCallback(response) {
                             $log.log(response);
                             $scope.bucketList = response.data.items
+                            $timeout.cancel(timeout);
                         }, function errorCallback(response) {
                             $log.log(response);
                             if (response.status === 401) {
-                                $log.log('User is not Authenticated')
-                                $window.location.href = '/';
+                                alert('You are not authenticated')
+                                $timeout.cancel(timeout);
                             }
                         });
                     timeout = $timeout(bucketListPoller, 2000);
@@ -30,7 +31,7 @@
                         .then(function successCallback(response) {
                             $log.log(response);
                             if (response.status == 200){
-                                $log.log('Bucket Created');
+                                getNewBucket(bucketKey)
                             }
                         }, function errorCallback(response) {
                             $log.log(response);
@@ -38,19 +39,45 @@
                 };
 
 
-                $scope.deleteBucket = function() {
-                    var bucketKey = $scope.bucketKey;
+                function getNewBucket(bucketKey) {
+                    var timeout = "";
+                    var poller = function() {
+                        $http.get('/api/buckets/' + bucketKey).
+                        then(function successCallback(response) {
+                            if (response.status === 202) {
+                                $log.log(response.data);
+                            } else if (response.status === 200) {
+                                $log.log(response.data);
+                                $scope.bucketList.push(response.data);
+                                $timeout.cancel(timeout);
+                                return false;
+                            }
+                            timeout = $timeout(poller, 2000);
+                        });
+                    };
+                    poller();
+                }
+
+                $scope.deleteBucket = function(bucketKey) {
                     $log.log('Deleting bucket: ' + bucketKey)
-                    $http.delete('/api/buckets/' + bucketKey)
-                        .then(function successCallback(response) {
-                            $log.log(response);
-                            if (response.status == 200){
-                                $log.log('Bucket Created');
-                            }
-                        }, function errorCallback(response) {
-                            $log.log(response);
-                        });
-                };
+                    var timeout = "";
+                    var poller = function() {
+                        $http.delete('/api/buckets/' + bucketKey)
+                            .then(function successCallback(response) {
+                                if (response.status === 202) {
+                                    $log.log(response.data);
+                                } else if (response.status === 200) {
+                                    $log.log(response.data);
+                                    bucketListPoller();
+                                    $timeout.cancel(timeout);
+                                    return false;
+                                }
+                                timeout = $timeout(poller, 2000);
+                            });
+                    };
+                    poller();
+                }
+
 
             }
 
